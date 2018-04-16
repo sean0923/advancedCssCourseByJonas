a21db21f671e7cedc65dddd723026c265e060d21
Sean Hong <iamseanhong@gmail.com>
2018-04-15 21:35:45 -0600

    Style container

--- Project-02-Trvello/src/js/ReactRouter.js
+++ Project-02-Trvello/src/js/ReactRouter.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Header from './components/layout/Header';
+
+import Sidebar from './components/layout/Sidebar';
 import Home from './components/Home';
 
 class ReactRouter extends Component {
   // ---------------------------------------------------------------------------------------
   // ------------------------------------- Render ------------------------------------------
   // ---------------------------------------------------------------------------------------
-  render () {
+  render() {
     return (
       <BrowserRouter>
-        <div>
-          <Switch>
-            <Route path={'/'} component={Home} />
-          </Switch>
+        <div className="container">
+          <Header />
+          <div className="content">
+            <Sidebar />
+
+            <Switch>
+              <Route path={'/'} component={Home} />
+            </Switch>
+          </div>
         </div>
       </BrowserRouter>
     );
