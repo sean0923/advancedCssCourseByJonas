16cfed1674779ab0bb200bcf101746f3b15cc35c
Sean Hong <iamseanhong@gmail.com>
2018-04-15 20:51:31 -0600

    Initial setup for Project-02

---
+++ Project-02-Trvello/src/js/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import ReactRouter from './ReactRouter';
+
+render(<ReactRouter />, document.getElementById('app'));
