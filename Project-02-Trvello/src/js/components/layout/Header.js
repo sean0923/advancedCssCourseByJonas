7aef729adeefa320ac07ea62afb0b05eba305b43
Sean Hong <iamseanhong@gmail.com>
2018-04-16 20:57:32 -0700

    Able to use svg with sprite.svg

--- Project-02-Trvello/src/js/components/layout/Header.js
+++ Project-02-Trvello/src/js/components/layout/Header.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import SvgIcon from '../SvgIcon';
+import logo from '../../../../assets/logo.png';
+
 
 export default class Header extends Component {
   render() {
     return (
       <div className="header">
+        <img src={logo} alt="logo" />
         Header
       </div>
-    )
+    );
   }
 }
