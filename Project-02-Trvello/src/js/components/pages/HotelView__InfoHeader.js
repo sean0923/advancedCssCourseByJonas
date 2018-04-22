984f3c6920b8b22180e9b424e2e80bbb5f7b4457
Sean Hong <iamseanhong@gmail.com>
2018-04-21 17:37:01 -0700

    Clean up code

--- Project-02-Trvello/src/js/components/pages/HotelView__InfoHeader.js
+++ Project-02-Trvello/src/js/components/pages/HotelView__InfoHeader.js
@@ -15,82 +15,119 @@ const temp = `
 `;
 
 const Wrapper = styled.div`
-  ${temp};
   display: flex;
   justify-content: space-between;
+
+  background-color: #fff;
 `;
 
-const LeftBox = styled.div`
-  ${temp};
+const BoxLeft = styled.div`
   display: flex;
+
+  & > * {
+    padding: 1rem 2rem;
+    display: flex;
+    align-items: center;
+  }
 `;
 
-const RightBox = styled.div`
-  ${temp};
+const BoxRight = styled.div`
   display: flex;
+
+  & > * {
+    padding: 1rem 2rem;
+  }
 `;
 
-const HotelName = styled.div`
-  ${temp};
+const BoxLeft_HotelName = styled.div`
   text-transform: uppercase;
   text-decoration: bold;
   font-size: 2rem;
-  padding: 1rem 2rem;
 `;
 
-const StarBox = styled.div`
-  ${temp};
-  padding: 1rem 2rem;
+const BoxLeft_BoxStar = styled.div`
+  display: flex;
+`;
+
+const BoxRight_BoxLocation = styled.div`
+  color: var(--color-primary);
+  text-decoration: underline;
 
   display: flex;
   align-items: center;
+
+  & > *:not(:last-child) {
+    margin-right: .5rem;
+  }
+
+  cursor: pointer;
+  &:hover {
+    color: var(--color-primary-dark);
+  }
 `;
 
-const LocationBox = styled.div`
-  ${temp};
-
+const BoxRight_BoxRate = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
+  justify-content: center;
+  color: var(--color-grey-light-1);
+  background: var(--color-primary);
 `;
 
-const RateBox = styled.div`
-  ${temp};
+const TextRate = styled.div`font-size: 1.8rem;`;
 
-  display: flex;
-  align-items: center;
-`;
+const TextVote = styled.div`font-size: .8rem;`;
+
+const TextLocation = styled.div`font-size: 1.4rem;`;
 
 // -----------------------------------------------------------------------------------------
 // ------------------------------------ Styled Svg Icons  ----------------------------------
 // -----------------------------------------------------------------------------------------
-const SytledSvgIcon = styled(SvgIcon)`
+const StyledSvgIcon = styled(SvgIcon)`
   height: var(--size-svg-at-header-hotel-info);
   width: var(--size-svg-at-header-hotel-info);
+  fill: currentColor;  
+`;
+
+const StyledStarSvgIcon = styled(StyledSvgIcon)`
   fill: var(--color-primary);  
 `;
 
 export default class HotelView__InfoHeader extends Component {
+  // ---------------------------------------------------------------------------------------
+  // ------------------------------------ Sub Render ---------------------------------------
+  // ---------------------------------------------------------------------------------------
+  renderStars() {
+    return (
+      <BoxLeft_BoxStar>
+        {[...Array(5).keys()].map((num, idx) => {
+          return <StyledStarSvgIcon key={idx} icon="star" />;
+        })}
+      </BoxLeft_BoxStar>
+    );
+  }
+
   // ---------------------------------------------------------------------------------------
   // ------------------------------------ Render -------------------------------------------
   // ---------------------------------------------------------------------------------------
   render() {
     return (
       <Wrapper>
-        <LeftBox>
-          <HotelName> Hotel Name </HotelName>
-          <StarBox>
-            {[...Array(5).keys()].map((num, idx) => {
-              return <SytledSvgIcon key={idx} icon="star" />;
-            })}
-          </StarBox>
-        </LeftBox>
-        <RightBox>
-          <LocationBox>
-            <div>svg icon</div>
-            <div>Seoul Korea</div>
-          </LocationBox>
-          <RateBox>RateBox</RateBox>
-        </RightBox>
+        <BoxLeft>
+          <BoxLeft_HotelName> Hotel Name </BoxLeft_HotelName>
+          {this.renderStars()}
+        </BoxLeft>
+        <BoxRight>
+          <BoxRight_BoxLocation>
+            <StyledSvgIcon icon="location-pin" />
+            <TextLocation>Seoul, Korea</TextLocation>
+          </BoxRight_BoxLocation>
+          <BoxRight_BoxRate>
+            <TextRate>8.6</TextRate>
+            <TextVote>429 votes</TextVote>
+          </BoxRight_BoxRate>
+        </BoxRight>
       </Wrapper>
     );
   }
