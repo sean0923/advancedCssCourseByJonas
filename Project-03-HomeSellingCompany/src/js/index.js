16ed778eb8fbf989699608e765edd488426f1256
Sean Hong <iamseanhong@gmail.com>
2018-04-23 21:43:38 -0700

    Project-3 Initial app setup

---
+++ Project-03-HomeSellingCompany/src/js/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import AppLayout from './AppLayout';
+
+render(<AppLayout />, document.getElementById('app'));
