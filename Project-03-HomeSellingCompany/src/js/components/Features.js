26d8ab39d6eda3d249e8102267d1a4ba138d0467
Sean Hong <iamseanhong@gmail.com>
2018-04-25 21:32:40 -0700

    Non responsitve style

--- Project-03-HomeSellingCompany/src/js/components/Features.js
+++ Project-03-HomeSellingCompany/src/js/components/Features.js
@@ -12,24 +12,42 @@ import SvgIcon from '../common/SvgIcon';
 // ------------------------------------ Styled Components ----------------------------------
 // -----------------------------------------------------------------------------------------
 const Wrapper = styled.div`
-  background: var(--color-grey-light-2);
   grid-column: features-start / features-end;
 
   display: grid;
   grid-template-columns: repeat(3, 1fr);
+  grid-gap: 6rem;
 
   margin: 15rem 0;
 `;
 
-const H4 = styled.h4``;
+const ItemWrapper = styled.div`
+  display: grid;
+  grid-template-columns: min-content 1fr;
+  grid-column-gap: 3rem;
+`;
 
-const P = styled.p``;
+const H4 = styled.h4`
+  font-family: var(--font-display);
+  font-weight: 400;
+  font-size: 1.9rem;
+  color: var(--color-grey-dark-1);
+
+  margin-bottom: 1.25rem;
+`;
+
+const P = styled.p`
+  font-family: var(--font-primary);
+  font-size: 1.7rem;
+`;
 // -----------------------------------------------------------------------------------------
 // ------------------------------------ Styled Svg Icon ------------------------------------
 // -----------------------------------------------------------------------------------------
 const StyledSvgIcon = styled(SvgIcon)`
   fill: var(--color-primary-dark);
-  width: 3rem;
+  width: 4.5rem;
+  height: 4.5rem;
+  transform: translateY(-1.25rem);
 `;
 
 // -----------------------------------------------------------------------------------------
@@ -41,12 +59,13 @@ const Features = () => {
     <Wrapper>
       {dataForFeatures.map(({ text, iconName, description }) => {
         return (
-          <div>
+          <ItemWrapper>
             <StyledSvgIcon icon={iconName} />
-            <H4>{text}</H4>
-            <div>{iconName}</div>
-            <P>{description}</P>
-          </div>
+            <div>
+              <H4>{text}</H4>
+              <P>{description}</P>
+            </div>
+          </ItemWrapper>
         );
       })}
     </Wrapper>
