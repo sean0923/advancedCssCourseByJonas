1783ae40fc11e6d6c8e0b70130a822f62f177d83
Sean Hong <iamseanhong@gmail.com>
2018-04-29 19:16:00 -0600

    Render data related to home section

--- Project-03-HomeSellingCompany/src/js/components/Homes.data.js
+++ Project-03-HomeSellingCompany/src/js/components/Homes.data.js
@@ -1,3 +1,5 @@
+import faker from 'faker';
+
 import imgSrcHouse1 from '../../../assets/house-1.jpeg';
 import imgSrcHouse2 from '../../../assets/house-2.jpeg';
 import imgSrcHouse3 from '../../../assets/house-3.jpeg';
@@ -5,21 +7,61 @@ import imgSrcHouse4 from '../../../assets/house-4.jpeg';
 import imgSrcHouse5 from '../../../assets/house-5.jpeg';
 import imgSrcHouse6 from '../../../assets/house-6.jpeg';
 
-const NAME_ICON_HEART_FULL = 'heart-full';
-const NAME_ICON_KEY = 'key';
+export const NAME_ICON_HEART_FULL = 'heart-full';
+export const NAME_ICON_KEY = 'key';
+export const NAME_ICON_MAP_PIN = 'map-pin';
+export const NAME_ICON_PROFILE_MAIL = 'profile-male';
 
-const TEXT_HOUSE_1 = 'HOUSE';
-const TEXT_HOUSE_2 = 'HOUSE';
-const TEXT_HOUSE_3 = 'HOUSE';
+const TEXT_HOUSE_1 = 'Beautiful Family House';
+const TEXT_HOUSE_2 = 'Modern Glass Villa';
+const TEXT_HOUSE_3 = 'Cosy Country House';
 const TEXT_HOUSE_4 = 'HOUSE';
 const TEXT_HOUSE_5 = 'HOUSE';
 const TEXT_HOUSE_6 = 'HOUSE';
 
+faker.random.number();
+
 export default [
-  { text: TEXT_HOUSE_1, imgSrc: imgSrcHouse1, icon: NAME_ICON_HEART_FULL },
-  { text: TEXT_HOUSE_2, imgSrc: imgSrcHouse2, icon: NAME_ICON_KEY },
-  { text: TEXT_HOUSE_3, imgSrc: imgSrcHouse3, icon: NAME_ICON_KEY },
-  { text: TEXT_HOUSE_4, imgSrc: imgSrcHouse4, icon: NAME_ICON_KEY },
-  { text: TEXT_HOUSE_5, imgSrc: imgSrcHouse5, icon: NAME_ICON_KEY },
-  { text: TEXT_HOUSE_6, imgSrc: imgSrcHouse6, icon: NAME_ICON_KEY },
+  {
+    text: TEXT_HOUSE_1,
+    imgSrc: imgSrcHouse1,
+    nameCountry: 'USA',
+    numArea: faker.random.number({ min: 325, max: 1000 }),
+    numDollar: faker.random.number({ min: 1, max: 10 }),
+  },
+  {
+    text: TEXT_HOUSE_2,
+    imgSrc: imgSrcHouse2,
+    nameCountry: 'UK',
+    numArea: faker.random.number({ min: 325, max: 1000 }),
+    numDollar: faker.random.number({ min: 1, max: 10 }),
+  },
+  {
+    text: TEXT_HOUSE_3,
+    imgSrc: imgSrcHouse3,
+    nameCountry: 'CANADA',
+    numArea: faker.random.number({ min: 325, max: 1000 }),
+    numDollar: faker.random.number({ min: 1, max: 10 }),
+  },
+  {
+    text: TEXT_HOUSE_4,
+    imgSrc: imgSrcHouse4,
+    nameCountry: 'KOREA',
+    numArea: faker.random.number({ min: 325, max: 1000 }),
+    numDollar: faker.random.number({ min: 1, max: 10 }),
+  },
+  {
+    text: TEXT_HOUSE_5,
+    imgSrc: imgSrcHouse5,
+    nameCountry: 'JAPAN',
+    numArea: faker.random.number({ min: 325, max: 1000 }),
+    numDollar: faker.random.number({ min: 1, max: 10 }),
+  },
+  {
+    text: TEXT_HOUSE_6,
+    imgSrc: imgSrcHouse6,
+    nameCountry: 'CHINA',
+    numArea: faker.random.number({ min: 325, max: 1000 }),
+    numDollar: faker.random.number({ min: 1, max: 10 }),
+  },
 ];
--- Project-03-HomeSellingCompany/src/js/components/Homes.js
+++ Project-03-HomeSellingCompany/src/js/components/Homes.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import { format } from 'money-formatter';
 
 // -----------------------------------------------------------------------------------------
 // ------------------------------------ Data -----------------------------------------------
 // -----------------------------------------------------------------------------------------
-import dataHomes from './Homes.data';
+import dataHomes, {
+  NAME_ICON_HEART_FULL,
+  NAME_ICON_KEY,
+  NAME_ICON_MAP_PIN,
+  NAME_ICON_PROFILE_MAIL,
+} from './Homes.data';
+
+// -----------------------------------------------------------------------------------------
+// ------------------------------------ SvgIcon --------------------------------------------
+// -----------------------------------------------------------------------------------------
+import SvgIcon from '../common/SvgIcon';
 
 // -----------------------------------------------------------------------------------------
 // ------------------------------------ Styled Components ----------------------------------
@@ -24,15 +35,31 @@ const Img = styled.img`
 `;
 
 const Text = styled.div``;
+const Btn = styled.button`text-transform: uppercase;`;
+
+// -----------------------------------------------------------------------------------------
+// ------------------------------------ Styled SvgIcon -------------------------------------
+// -----------------------------------------------------------------------------------------
+const StyledSvgIcon = styled(SvgIcon)`
+  width: 3rem;
+  fill: var(--color-primary-dark);
+`;
 
 const Homes = () => {
   return (
     <Wrapper>
-      {dataHomes.map(({ text, imgSrc, icon }, idx) => {
+      {dataHomes.map(({ text, imgSrc, nameCountry, numArea, numDollar }, idx) => {
         return (
-          <div>
+          <div key={idx}>
             <Img src={imgSrc} key={`a-${idx}`} />
+            <StyledSvgIcon icon={NAME_ICON_HEART_FULL} />
             <Text key={`b-${idx}`}>{text}</Text>
+            <div>{nameCountry}</div>
+            <div>
+              {numArea}m<sup>2</sup>
+            </div>
+            <div>{format('USD', numDollar * 1000000, 0)}</div>
+            <Btn>contact relator</Btn>
           </div>
         );
       })}
