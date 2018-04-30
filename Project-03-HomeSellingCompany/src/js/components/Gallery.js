15dd444f2759494026d0dbe10414bd1e868b250b
Sean Hong <iamseanhong@gmail.com>
2018-04-29 20:44:25 -0600

    Use object-fit:cover to fit img to grid

--- Project-03-HomeSellingCompany/src/js/components/Gallery.js
+++ Project-03-HomeSellingCompany/src/js/components/Gallery.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import srcGal_1 from '../../../assets/gal-1.jpeg';
+
 // -----------------------------------------------------------------------------------------
 // ------------------------------------ Styled Components ----------------------------------
 // -----------------------------------------------------------------------------------------
 const Wrapper = styled.div`
-  background: var(--color-secondary);
+  background: var(--color-grey-light-1);
   grid-column: full-start / full-end;
+
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(7, 5vw);
+  padding: 1.5rem;
+  grid-gap: 1.5rem;
+`;
+
+const ImgWrapper = styled.div`
+  grid-row: 1 / span 2;
+  grid-column: 1 / span 2;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  display: block;
 `;
 
 const Gallery = () => {
   return (
     <Wrapper>
-      Gallery
+      <ImgWrapper>
+        <Img src={srcGal_1} alt={'gal img 1'} />
+      </ImgWrapper>
     </Wrapper>
   );
 };
