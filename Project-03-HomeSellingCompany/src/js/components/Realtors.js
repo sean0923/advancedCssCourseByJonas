c96367fc5ae2df5f9d279056f71950528702c338
Sean Hong <iamseanhong@gmail.com>
2018-05-02 21:28:46 -0700

    Spell error realtors

--- Project-03-HomeSellingCompany/src/js/components/Realtors.js
+++ Project-03-HomeSellingCompany/src/js/components/Realtors.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import faker from 'faker';
 
-import srcRelator1 from '../../../assets/realtor-1.jpeg';
-import srcRelator2 from '../../../assets/realtor-2.jpeg';
-import srcRelator3 from '../../../assets/realtor-3.jpeg';
+import srcRealtor1 from '../../../assets/realtor-1.jpeg';
+import srcRealtor2 from '../../../assets/realtor-2.jpeg';
+import srcRealtor3 from '../../../assets/realtor-3.jpeg';
 
 // -----------------------------------------------------------------------------------------
 // ------------------------------------ Styled Components ----------------------------------
@@ -31,7 +31,7 @@ const SectionWrapper = styled.div`
   }
 `;
 
-const RelatorWrapper = styled.div`
+const Realtor = styled.div`
   display: grid;
   grid-template-columns: auto 1fr;
   grid-gap: 2rem;
@@ -42,7 +42,7 @@ const RelatorWrapper = styled.div`
   }
 `;
 
-const RelatorInfo = styled.div`
+const RealtorInfo = styled.div`
   display: grid;
   justify-content: start;
   align-content: center;
@@ -61,24 +61,24 @@ const RelatorInfo = styled.div`
   }
 `;
 
-const threeRelators = [srcRelator1, srcRelator2, srcRelator3];
+const threeRealtors = [srcRealtor1, srcRealtor2, srcRealtor3];
 
 const Realtors = () => {
   return (
     <Wrapper>
       <SectionWrapper>
-        <div> TOP 3 RELATORS </div>
-        {threeRelators.map((src, idx) => {
+        <div> TOP 3 REALTORS </div>
+        {threeRealtors.map((src, idx) => {
           const firstName = faker.name.firstName();
           const lastName = faker.name.lastName();
           return (
-            <RelatorWrapper key={idx}>
+            <Realtor key={idx}>
               <img src={src} alt={`relatro-${idx + 1}`} />
-              <RelatorInfo>
+              <RealtorInfo>
                 <div>{`${firstName} ${lastName}`}</div>
                 <div> {`${faker.random.number({ min: 100, max: 500 })} HOUSE SOLD`}</div>
-              </RelatorInfo>
-            </RelatorWrapper>
+              </RealtorInfo>
+            </Realtor>
           );
         })}
       </SectionWrapper>
